b77575d58c328276ba8d894b4752107e4db72e3f
elexonics <renaissance.wangzr@gmail.com>
2014-10-29 20:15:46 -0500

    add new function in Experiment.py

--- Experiment.py
+++ Experiment.py
@@ -1,11 +1,25 @@
 import sys
 
-def largest(list):
-  if len(list) == 0:
-    raise ValueError("Cannot call largest on empty list")
-  max = -sys.maxint # "smallest" possible int
-  # max = 0
-  for index in range(len(list)):
-    if (list[index] > max):
-      max = list[index]
-  return max
+def largest(inlist):
+	if len(inlist) == 0:
+		raise ValueError("Cannot call largest on empty list")
+  	max = -sys.maxint # "smallest" possible int
+  	# max = 0
+  	for index in range(len(inlist)):
+  		if (inlist[index] > max):
+  			max = inlist[index]
+  			return max
+
+def average(inlist):
+	if len(inlist) == 0:
+		raise ValueError("Cannot call average on empty list")
+
+	sum = 0
+	for num in inlist:
+		try:
+			f_num = float(num)
+		except ValueError:
+			raise ValueError("Non-number element in list")
+		else:
+			sum += f_num
+	return sum/len(inlist)
